3b96e9260e888918d4348ea410a1a03c573d8080
Nirmal kumar Ravi <nirmal.kumar@royalcyber.com>
2018-01-07 21:12:02 -0600

    regex

--- leethcode/src/leethcode/hard/RegularExpressionMatching.java
+++ leethcode/src/leethcode/hard/RegularExpressionMatching.java
@@ -2,54 +2,69 @@ package leethcode.hard;
 
 public class RegularExpressionMatching {
 	
-	
-	private void print(Integer[][] mem){
-		System.out.println();
-		for(int i=0;i< mem.length;i++){
-			System.out.println();
-			for(int j =0;j<mem[i].length;j++){
-				System.out.print(mem[i][j]+"\t");
-				
-			}
-		}
+	private  boolean isMatch(char[] s, char[] p, int sPtr, int pPtr, Boolean mem[][]){
 		
-		
-	}
-	
-	private boolean isMatch(char[] s, char[] p, int i, int j, Character c,Integer[][] mem){
-		print(mem);
-		if(i <0 && j < 0){
+		if(sPtr < 0 && pPtr <0){
 			return true;
-		}else if( i < 0 || j < 0){
-			//System.out.println(i+" "+j);
+		}else if(pPtr < 0 || sPtr <0){
 			return false;
 		}else{
-			if(mem[i][j] != null){
-				return mem[i][j] == 0;
-			}else{
-				boolean res = false;
-				if(s[i] == p[j] || p[j] == '.'){
-					res=isMatch(s,p,i-1,j-1,c,mem);
-				}else if(p[j] == '*'){
-					c = (c==null)? p[j-1]: c;
-					if(c== s[i] || c=='.'){//there is a match
-						res = isMatch(s,p,i-1,j,c,mem) || isMatch(s,p,i-1,j-2,null,mem)|| isMatch(s,p,i,j-2,null,mem);
-						//System.out.println(res);
-					}else{
-						res = isMatch(s,p,i,j-2,null,mem);
-					}
-				}else{
-					res = false;
+			if(mem[sPtr][pPtr] != null){
+				return mem[sPtr][pPtr];
+			}
+			boolean match = false;
+			boolean zeroMatch = false;
+			boolean oneOrMoreMatch = false;
+			if(s[sPtr] == p[pPtr] || p[pPtr] == '.'){
+				match = isMatch(s,p,sPtr-1,pPtr-1);
+			}else if(p[pPtr] == '*'){
+				//0-Match
+				zeroMatch = isMatch(s,p,sPtr,pPtr-2);
+				//1 or more match match
+				if(s[sPtr] == p[pPtr-1] || p[pPtr-1] == '.'){
+					oneOrMoreMatch = isMatch(s,p,sPtr-1,pPtr) || isMatch(s,p,sPtr-1,pPtr-2);
 				}
-				mem[i][j] = (res)?0:1;
-				return mem[i][j] == 0;
-			}	
+			}
+			mem[sPtr][pPtr] =  false || match || zeroMatch || oneOrMoreMatch;
+			return mem[sPtr][pPtr];
+		}
+		
+		
+	}
+	
+	private boolean isMatch(char[] s, char[] p, int sPtr, int pPtr){
+		if(sPtr < 0 && pPtr <0){
+			return true;
+		}else if(pPtr < 0 || sPtr <0){
+			return false;
+		}else{
+			//System.out.println(sPtr +" "+pPtr);
+			boolean match = false;
+			boolean zeroMatch = false;
+			boolean oneOrMoreMatch = false;
+			if(s[sPtr] == p[pPtr] || p[pPtr] == '.'){
+				match = isMatch(s,p,sPtr-1,pPtr-1);
+			}else if(p[pPtr] == '*'){
+				//0-Match
+				zeroMatch = isMatch(s,p,sPtr,pPtr-2);
+				//1 or more match match
+				if(s[sPtr] == p[pPtr-1] || p[pPtr-1] == '.'){
+					oneOrMoreMatch = isMatch(s,p,sPtr-1,pPtr) || isMatch(s,p,sPtr-1,pPtr-2);
+				}
+			}
+			return false || match || zeroMatch || oneOrMoreMatch;
+			
 		}
 	}
 	
+	
 	public boolean isMatch(String s, String p) {
-		Integer[][] mem = new Integer[s.length()][p.length()];
-		return isMatch(s.toCharArray(),p.toCharArray(),s.length()-1,p.length()-1,null,mem);
+		Boolean[][] mem = new Boolean[s.length()][p.length()];
+		//append x to handle empty case
+		s= "x"+s;
+		p= "x"+p;
+		return isMatch(s.toCharArray(), p.toCharArray(), s.length()-1,p.length()-1,mem);
+		//return true;
     }
 
 	public static void main(String[] args) {
@@ -63,10 +78,8 @@ public class RegularExpressionMatching {
 		System.out.println(rgx.isMatch("aa", ".*"));
 		System.out.println(rgx.isMatch("ab", ".*"));*/
 		System.out.println(rgx.isMatch("aab", "c*a*b"));
-		/*System.out.println(rgx.isMatch("ab", ".*c"));
-		System.out.println(rgx.isMatch("aaa", ".a*a"));
-		System.out.println(rgx.isMatch("a", "ab*"));*/
-
+		
+		//System.out.println(rgx.isMatch("aaa", "aaaa"));
 	}
 
 }
